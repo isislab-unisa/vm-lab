36e879541f01fb3e397d343e19306d43fabdf7c8
AlfredoJSpera <alfredo.j.spera@gmail.com>
2024-12-21 12:21:41 +0100

    feat(frontend): Add confirm in page function

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+from typing import Callable
+
 import streamlit as st
 from streamlit import switch_page
 
-from frontend.custom_components import confirm_dialog
+from frontend.custom_components import confirm_dialog, confirm_in_page
 from frontend.page_names import PageNames
 
 #switch_page(PageNames.my_vms)
-if st.button("Go to my vms"):
+if st.button("Go to my vms", type="primary"):
 	switch_page(PageNames.my_vms)
 
 
@@ -13,9 +15,17 @@ def my_printer(something):
 	print(something)
 
 
-if st.button("Confirm"):
+if st.button("Confirm in page"):
+	confirm_in_page(
+		confirm_button_callback=lambda: my_printer("Yes in page"),
+		cancel_button_callback=lambda: my_printer("No in page"),
+	)
+
+
+if st.button("Confirm dialog"):
 	confirm_dialog(
-		confirm_button_callback=lambda: my_printer("Yes"),
+		confirm_button_callback=lambda: my_printer("Yes in dialog"),
+		cancel_button_callback=lambda: my_printer("No in dialog"),
 	)
 
 
--- frontend/custom_components.py
+++ frontend/custom_components.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 from typing import Callable, Union, Literal
 
+from plotly.validators.pointcloud.marker import border
+
 from backend.models import User, VirtualMachine, Bookmark
 from backend.role import Role
 from frontend.page_names import PageNames
@@ -169,6 +171,53 @@ def render_sidebar_menu(role: Role | None):
 				st.page_link(PageNames.forgot_credentials, label="Forgot Credentials")
 
 
+def confirm_in_page(
+		bordered_container: bool = True, text: str = None, caption: str = None,
+		confirm_button_label: str = "Yes", cancel_button_label: str = "Cancel",
+		is_confirm_button_type_primary: bool = False, is_cancel_button_type_primary: bool = False,
+		confirm_button_callback: Callable = None, cancel_button_callback: Callable = None
+):
+	"""
+	https://discuss.streamlit.io/t/add-cancel-button-to-close-modal/87318
+	Renders a confirmation *section* with a description and two buttons with custom behavior.
+
+	:param bordered_container: Whether to render the container with a border
+	:param text: The description displayed on top of the dialog (not the title)
+	:param caption: The caption (muted text) displayed below the description
+	:param confirm_button_label: The label for the button to confirm
+	:param cancel_button_label: The label for the button to cancel
+	:param is_confirm_button_type_primary: Whether to render the confirm button as primary
+	:param is_cancel_button_type_primary: Whether to render the cancel button as primary
+	:param confirm_button_callback: The callback for the confirm button
+	:param cancel_button_callback: The callback for the cancel button
+	"""
+	with st.container(border=bordered_container):
+		if text:
+			st.markdown(text)
+		if caption:
+			st.caption(caption)
+
+		yes_button_type = "secondary"
+		if is_confirm_button_type_primary:
+			yes_button_type = "primary"
+
+		cancel_button_type = "secondary"
+		if is_cancel_button_type_primary:
+			cancel_button_type = "primary"
+
+		yes_column, cancel_column = st.columns(2)
+
+		with yes_column:
+			if st.button(confirm_button_label, use_container_width=True, type=yes_button_type,
+					  on_click=confirm_button_callback):
+				st.rerun()
+
+		with cancel_column:
+			if st.button(cancel_button_label, use_container_width=True, type=cancel_button_type,
+					  on_click=cancel_button_callback):
+				st.rerun()
+
+
 @st.dialog("Confirm")
 def confirm_dialog(
 		text: str = None, caption: str = None,
@@ -177,9 +226,10 @@ def confirm_dialog(
 		confirm_button_callback: Callable = None, cancel_button_callback: Callable = None
 ):
 	"""
-	Renders a confirmation dialog with two buttons with custom behavior.
+	https://discuss.streamlit.io/t/add-cancel-button-to-close-modal/87318
+	Renders a confirmation *dialog* with a description and two buttons with custom behavior.
 
-	NOTE: This does not work when called inside another dialog. Use TODO:Create function
+	NOTE: This does not work when called inside another dialog. Use `confirm_in_page`.
 
 	:param text: The description displayed on top of the dialog (not the title)
 	:param caption: The caption (muted text) displayed below the description
@@ -190,29 +240,15 @@ def confirm_dialog(
 	:param confirm_button_callback: The callback for the confirm button
 	:param cancel_button_callback: The callback for the cancel button
 	"""
-	if text:
-		st.markdown(text)
-	if caption:
-		st.caption(caption)
+	confirm_in_page(
+		bordered_container=False,
+		text=text,
+		caption=caption,
+		confirm_button_label=confirm_button_label,
+		cancel_button_label=cancel_button_label,
+		is_confirm_button_type_primary=is_confirm_button_type_primary,
+		is_cancel_button_type_primary=is_cancel_button_type_primary,
+		confirm_button_callback=confirm_button_callback,
+		cancel_button_callback=cancel_button_callback
+	)
 
-	yes_button_type = "secondary"
-	if is_confirm_button_type_primary:
-		yes_button_type = "primary"
-
-	cancel_button_type = "secondary"
-	if is_cancel_button_type_primary:
-		cancel_button_type = "primary"
-
-	yes_column, cancel_column = st.columns(2)
-
-	with yes_column:
-		if st.button(confirm_button_label, use_container_width=True, type=yes_button_type):
-			if confirm_button_callback is not None:
-				confirm_button_callback()
-			st.rerun()
-
-	with cancel_column:
-		if st.button(cancel_button_label, use_container_width=True, type=cancel_button_type):
-			if cancel_button_callback is not None:
-				cancel_button_callback()
-			st.rerun()
