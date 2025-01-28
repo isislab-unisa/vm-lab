84f2cf29cf63495707ce348553cf4b414aedb83c
AlfredoJSpera <alfredo.j.spera@gmail.com>
2025-01-28 10:46:28 +0100

    refactor: Move interactive data table function to frontend

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from typing import Callable
-
 import streamlit as st
 from streamlit import switch_page
 
 from backend.database import get_db
 from backend.models import VirtualMachine
+from frontend.custom_components import interactive_data_table
 from frontend.page_names import PageNames
 
 #switch_page(PageNames.my_vms)
@@ -35,108 +34,6 @@ def my_mock_db():
 	return result
 
 
-def better_table(data, column_settings, button_settings, title="Table",
-				 refresh_data_callback: Callable = None, clear_filters_button: bool = True):
-	"""
-	:param data: A list of dictionaries containing the data to display.
-	:param column_settings: Configuration of the columns. Each key is the column title, and the value is a dictionary with:
-	    - "column_width": The width of the column.
-	    - "data_name": The name of the field in the data dictionary.
-	:param button_settings: Configuration of the buttons. Each key is the button name, and the value is a dictionary with:
-	    - "primary": Indicates if the button is primary.
-	    - "callback": The callback function to be called when the button is clicked.
-	"""
-
-	# Write the title
-	st.write(f"### {title}")
-	st.write("---")
-
-	# List with column names
-	display_names = list(column_settings.keys())
-
-	# List with column widths
-	widths = []
-	for name in display_names:
-		widths.append(column_settings[name]["column_width"])
-
-	# Default filtered data is all the data
-	filtered_data = data
-
-	# Filters
-	with st.expander("Filters"):
-		filters_buttons_col1, filters_buttons_col2 = st.columns(2)
-
-		# Refresh/Clear buttons
-		with filters_buttons_col1:
-			if refresh_data_callback is not None and st.button(":material/Refresh: Refresh Data", use_container_width=True):
-				st.cache_data.clear()
-				filtered_data = refresh_data_callback()
-
-		with filters_buttons_col2:
-			if clear_filters_button and st.button(":material/Clear_All: Clear Filters", use_container_width=True):
-				st.session_state[f"{title}-search_selectbox"] = display_names[0]
-				st.session_state[f"{title}-search_query"] = ""
-
-		# Search Bars
-		search_column = st.selectbox("Select column to search",
-									 display_names,
-									 key=f"{title}-search_selectbox")
-		search_query = st.text_input("Search",
-									 "",
-									 key=f"{title}-search_query")
-
-		# Search in the entire data list
-		if search_query:
-			data_name_to_search = column_settings[search_column]["data_name"]
-			filtered_data = []
-			for row in data:
-				if search_query.lower() in str(row[data_name_to_search]).lower():
-					filtered_data.append(row)
-
-	# Write the Header Row
-	columns_header = st.columns(widths + [1])
-
-	for index, name in enumerate(display_names):
-		columns_header[index].write(f"**{name}**")
-
-	columns_header[-1].write("**Actions**")
-
-	# Write all the Rows
-	for data_index, data_row in enumerate(filtered_data):
-		columns_row = st.columns(widths + [1])
-
-		# Data Columns
-		for index, name in enumerate(display_names):
-			data_name = column_settings[name]["data_name"]
-			columns_row[index].write(data_row[data_name])
-
-		# Action Buttons Column
-		with columns_row[-1]:
-			for button_label, button_configuration in button_settings.items():
-				button_type = "primary" if button_configuration.get("primary", False) else "secondary"
-				button_disabled = data_row["buttons_disabled"].get(button_label, False)
-
-				button_icon = button_configuration.get("icon", "")
-				show_only_icon = button_configuration.get("show_only_icon", False)
-
-				button_help = button_configuration.get("help", None)
-
-				if show_only_icon and button_icon != "":
-					button_label_to_show = button_icon # Show only the icon
-				elif show_only_icon and button_icon == "":
-					button_label_to_show = button_label # There is no icon to show, so show only the label
-				else:
-					button_label_to_show = f"{button_icon} {button_label}" # Show icon and label
-
-				if st.button(key=f"{title}_{button_label}_{data_index}",
-							 label=button_label_to_show,
-							 type=button_type,
-							 disabled=button_disabled,
-							 help=button_help):
-					if not button_disabled:
-						button_configuration["callback"](data_row=data_row)
-
-
 @st.dialog("Edit")
 def edit_callback(data_row):
 	st.write(f"**Editing** user {data_row['name']} with ID {data_row['id']}")
@@ -150,7 +47,7 @@ def connect_callback(data_row):
 	st.write(f"**Calling** user {data_row['name']} with ID {data_row['id']}")
 
 
-better_table(
+interactive_data_table(
 	data=my_mock_db(),
 	refresh_data_callback=my_mock_db,
 	column_settings={
--- frontend/custom_components.py
+++ frontend/custom_components.py
@@ -253,3 +253,119 @@ def confirm_dialog(
 		cancel_button_callback=cancel_button_callback
 	)
 
+
+def interactive_data_table(data: list[dict],
+						   column_settings: dict, button_settings: dict,
+						   refresh_data_callback: Callable = None, clear_filters_button: bool = True,
+						   title: str = None):
+	"""
+	Full documentation here:
+	https://github.com/isislab-unisa/vm-lab/wiki/Component-%E2%80%90-Interactive-Data-Table
+
+	:param data: The data to display in the table.
+    :param refresh_data_callback: A function to call when the Refresh button in the filters is pressed.
+	:param column_settings: It defines how should the columns be.
+	:param button_settings: It defines what buttons should be included for each row.
+	:param title: The title (in markdown) to display above the table.
+	:param clear_filters_button: Whether to display the "Clear Filters" button in the filters' menu.
+	:raises ValueError: If `data_name` is not defined in a `column_settings` entry.
+	"""
+
+	# Write the title
+	if title is not None:
+		st.write(f"{title}")
+		st.write("---")
+
+	# List with column names
+	display_names = list(column_settings.keys())
+
+	# List with column widths
+	widths = []
+	for name in display_names:
+		column_width: int = column_settings.get(name).get("column_width", 1)
+		widths.append(column_width)
+
+	# Default filtered data is all the data
+	filtered_data = data
+
+	# Filters
+	with st.expander("Filters"):
+		filters_buttons_col1, filters_buttons_col2 = st.columns(2)
+
+		# Refresh/Clear buttons
+		with filters_buttons_col1:
+			if refresh_data_callback is not None and st.button(":material/Refresh: Refresh Data", use_container_width=True):
+				st.cache_data.clear()
+				filtered_data = refresh_data_callback()
+
+		with filters_buttons_col2:
+			if clear_filters_button and st.button(":material/Clear_All: Clear Filters", use_container_width=True):
+				# Clear the inputs
+				st.session_state[f"{title}-search_selectbox"] = display_names[0]
+				st.session_state[f"{title}-search_query"] = ""
+
+		# Search Bars
+		search_column = st.selectbox("Select column to search",
+									 display_names,
+									 key=f"{title}-search_selectbox")
+		search_query = st.text_input("Search",
+									 "",
+									 key=f"{title}-search_query")
+
+		# Search in the entire data list
+		if search_query:
+			data_name_to_search: str | None = column_settings.get(search_column).get("data_name", None)
+			if data_name_to_search is None:
+				raise ValueError(f"data_name must be defined in column_settings for '{search_column}'")
+
+			filtered_data = []
+			for row in data:
+				if search_query.lower() in str(row[data_name_to_search]).lower():
+					filtered_data.append(row)
+
+	# Write the Header Row
+	columns_header = st.columns(widths + [1])
+
+	for index, name in enumerate(display_names):
+		columns_header[index].write(f"**{name}**")
+
+	columns_header[-1].write("**Actions**")
+
+	# Write all the Rows
+	for data_index, data_row in enumerate(filtered_data):
+		columns_row = st.columns(widths + [1])
+
+		# Data Columns
+		for index, name in enumerate(display_names):
+			data_name = column_settings.get(name).get("data_name", None)
+			if data_name is None:
+				raise ValueError(f"data_name must be defined in column_settings for '{name}'")
+
+			columns_row[index].write(data_row[data_name])
+
+		# Action Buttons Column
+		with columns_row[-1]:
+			for button_label, button_configuration in button_settings.items():
+				button_type = "primary" if button_configuration.get("primary", False) else "secondary"
+				button_disabled = data_row["buttons_disabled"].get(button_label, False)
+
+				button_icon = button_configuration.get("icon", "")
+				show_only_icon = button_configuration.get("show_only_icon", False)
+
+				button_help = button_configuration.get("help", None)
+
+				if show_only_icon and button_icon != "":
+					button_label_to_show = button_icon # Show only the icon
+				elif show_only_icon and button_icon == "":
+					button_label_to_show = button_label # There is no icon to show, so show only the label
+				else:
+					button_label_to_show = f"{button_icon} {button_label}" # Show icon and label
+
+				if st.button(key=f"{title}_{button_label}_{data_index}",
+							 label=button_label_to_show,
+							 type=button_type,
+							 disabled=button_disabled,
+							 help=button_help):
+					button_callback = button_configuration.get("callback", None)
+					if not button_disabled and button_callback is not None:
+						button_callback(data_row=data_row)
